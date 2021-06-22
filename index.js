fce40caae71ca758a2d86a70a1f9688d7bf1ec9f
Harish <harish.shakya@techaheadcorp.com>
2021-06-22 13:39:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+var path = require("path");
+const express = require("express");
+const app = express();
+const port = 3000;
+const staticPath = path.join(__dirname,"../express/public");
+console.log(staticPath);
+app.use(express.static(staticPath));
+
+app.get('/',(req,res)=>{
+    //console.log(staticPath);
+    
+    res.send();
+    //res.send("this is testing home page");
+})
+app.get('/about',(req,res)=>{
+    res.json(
+     [
+        {
+            id:1,
+            name: "Harish"
+        },
+        {
+            id:2,
+            name: "Harshit"
+        }
+    ]
+    );
+})
+app.get('/contact',(req,res)=>{
+    res.write("<h1>This is testing contact page</h1>");
+    res.send();
+})
+app.listen(port,()=>{
+ console.log(`server started on port ${port}`);
+})
